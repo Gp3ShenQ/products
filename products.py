9a2fe062015ddd8fd5a185bb80078c0d52b92bc5
ShenQ <gp3g4xup6@gmail.com>
2022-04-17 22:23:43 +0800

    new file

---
+++ products.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+    name = input('輸入商品名稱: ')
+    if name == 'q':
+        break
+    price = input('輸入商品價格: ')
+    
+    products.append([name, price])  #這種寫法
+    #等同於下面寫法
+    # p =[]
+    # p.append(name)
+    # p.append(price)
+    #----
+    # p = [name, price] #直接建立一格清單
+print(products)
